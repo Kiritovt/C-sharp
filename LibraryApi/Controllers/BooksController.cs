7fb8de18bc3388794ee78d7ad73b4bb22624c991
Kiritovt <celientodomenico53@gmail.com>
2026-09-10 16:38:36 +0200

    Add BookService and dependency injection

--- LibraryApi/Controllers/BooksController.cs
+++ LibraryApi/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using LibraryApi.Models;
 using LibraryApi.Dtos;
+using LibraryApi.Services;
 
 namespace LibraryApi.Controllers;
 
@@ -9,31 +10,39 @@ namespace LibraryApi.Controllers;
 
 public class BooksController : ControllerBase
 {
-
-    private static List<Book> BookList = new List<Book>
+    private readonly BookService _bookService; 
+    
+    public BooksController(BookService bookService)
     {
-        new Book {Id=1, Title= "How I Met Your Mother?", PublicationYear= 1998},
-        new Book{Id= 2, Title="Is this the truth?", PublicationYear=2012 }
-    };
+        _bookService = bookService;
+    }
+
+    // private static List<Book> BookList = new List<Book>
+    // {
+    //     new Book {Id=1, Title= "How I Met Your Mother?", PublicationYear= 1998},
+    //     new Book{Id= 2, Title="Is this the truth?", PublicationYear=2012 }
+    // };
     [HttpGet]
 
     public ActionResult<List<Book>> GetBooks([FromQuery]int? year)
     {
-        if(year != null)
-        {
-            List<Book> filtered = BookList.Where(b => b.PublicationYear == year).ToList();
-            return Ok(filtered);
-        } else
-        {
-            return Ok(BookList);
-        }
+        List<Book> books = _bookService.GetBooks(year);
+        return Ok(books);
+        // if(year != null)
+        // {
+        //     List<Book> filtered = BookList.Where(b => b.PublicationYear == year).ToList();
+        //     return Ok(filtered);
+        // } else
+        // {
+        //     return Ok(BookList);
+        // }
     }
 
     [HttpGet("{id}")]
 
     public ActionResult<Book> GetBook(int id)
     {
-        Book? filtered = BookList.FirstOrDefault(b => b.Id == id);
+        Book? filtered = _bookService.GetBookById(id);
 
         if (filtered != null)
         {
@@ -48,47 +57,67 @@ public class BooksController : ControllerBase
     [HttpPost]
     public ActionResult<Book> CreateBook([FromBody] CreateBookDto bookDto)
     {
-        Book book = new Book
-        {
-            Id = BookList.Max(b=>b.Id) + 1,
-            Title = bookDto.Title,
-            PublicationYear = bookDto.PublicationYear
-        };
+        Book book = _bookService.CreateBook(bookDto);
+        // Book book = new Book
+        // {
+        //     Id = BookList.Max(b=>b.Id) + 1,
+        //     Title = bookDto.Title,
+        //     PublicationYear = bookDto.PublicationYear
+        // };
         
-        BookList.Add(book);
+        // BookList.Add(book);
         return CreatedAtAction( nameof(GetBook), new { id = book.Id}, book );
     }
 
     [HttpPut("{id}")]
     public IActionResult UpdateBook([FromRoute] int id, [FromBody] UpdateBookDto bookDto)
     {
-        Book? filtered = BookList.FirstOrDefault(bl => bl.Id == id);
+        bool update = _bookService.UpdateBook(id, bookDto);
 
-        if (filtered != null)
+        if (update)
         {
-            filtered.Title = bookDto.Title;
-            filtered.PublicationYear = bookDto.PublicationYear;
             return NoContent();
         }
         else
         {
             return NotFound();
         }
+        // Book? filtered = BookList.FirstOrDefault(bl => bl.Id == id);
+
+        // if (filtered != null)
+        // {
+        //     filtered.Title = bookDto.Title;
+        //     filtered.PublicationYear = bookDto.PublicationYear;
+        //     return NoContent();
+        // }
+        // else
+        // {
+        //     return NotFound();
+        // }
     }
 
     [HttpDelete("{id}")]
     public IActionResult DeleteBook([FromRoute] int id)
     {
-        Book? filtered = BookList.FirstOrDefault(bl => bl.Id == id);
-        if(filtered != null)
+        bool delete = _bookService.DeleteBook(id);
+        if (delete)
         {
-            BookList.Remove(filtered);
             return NoContent();
         }
         else
         {
             return NotFound();
         }
+        // Book? filtered = BookList.FirstOrDefault(bl => bl.Id == id);
+        // if(filtered != null)
+        // {
+        //     BookList.Remove(filtered);
+        //     return NoContent();
+        // }
+        // else
+        // {
+        //     return NotFound();
+        // }
     }
 }
 
---
+++ LibraryApi/Services/BookService.cs
@@ -0,0 +1,86 @@
+
+using LibraryApi.Dtos;
+using LibraryApi.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Services;
+
+public class BookService
+{
+    private List<Book> _bookList = new List<Book>()
+    {
+        new Book {Id=1, Title= "How I Met Your Mother?", PublicationYear= 1998},
+        new Book{Id= 2, Title="Is this the truth?", PublicationYear=2012 }
+    };
+
+    public List<Book> GetBooks(int? year)
+    {
+        if (year == null)
+        {
+            return (_bookList);
+        }
+        else
+        {
+            List<Book> filtered = _bookList.Where(b => b.PublicationYear == year).ToList();
+            return (filtered);
+        }
+    }
+
+    public Book? GetBookById(int id)
+    {
+        Book? filtered = _bookList.FirstOrDefault(b => b.Id == id);
+        if (filtered != null)
+        {
+            return filtered;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Book CreateBook(CreateBookDto bookDto)
+    {
+        Book book = new Book
+        {
+            Id = _bookList.Max(b => b.Id) + 1,
+            Title = bookDto.Title,
+            PublicationYear = bookDto.PublicationYear
+        };
+
+        _bookList.Add(book);
+        return book;
+    }
+
+    public bool UpdateBook(int id, UpdateBookDto bookDto)
+    {
+        Book? filtered = _bookList.FirstOrDefault(bl => bl.Id == id);
+
+        if (filtered != null)
+        {
+            filtered.Title = bookDto.Title;
+            filtered.PublicationYear = bookDto.PublicationYear;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public bool DeleteBook(int Id)
+    {
+        Book? filtered = _bookList.FirstOrDefault(b => b.Id == Id);
+
+        if (filtered != null)
+        {
+            _bookList.Remove(filtered);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
